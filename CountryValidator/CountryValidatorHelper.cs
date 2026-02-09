75ba7f289b559fac17fa70efabdfb33d5beddbd0
Steve Aze <90449376+ledgersteve@users.noreply.github.com>
2026-02-09 14:00:48 +0000

    Static wrapper for what should have been a static class in the first place.

---
+++ CountryValidator/CountryValidatorHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace CountryValidation
+{
+	public static class CountryValidatorHelper 
+	{
+		// The CountryValidator class has no state and should have been a static in the first place,
+		// but to avoid breaking changes, and to avoid getting too far adrift from the repo that
+		// we are forked from, we will keep it as is and wrap an instance.
+
+		private static readonly CountryValidator _instance = new CountryValidator();
+
+		public static bool IsCountrySupported(Country country)
+			=> CountryValidator.IsCountrySupported(country);
+
+
+		public static List<string> SupportedCountries
+			=> CountryValidator.SupportedCountries;
+
+		public static ValidationResult ValidateIndividualTaxCode(string ssn, Country country)
+			=> _instance.ValidateIndividualTaxCode(ssn, country);
+		public static ValidationResult ValidateVAT(string vat, Country country)
+			=> _instance.ValidateVAT(vat, country);
+
+		public static ValidationResult ValidateEntity(string vat, Country country)
+			=> _instance.ValidateEntity(vat, country);
+
+		public static ValidationResult ValidateNationalIdentityCode(string ssn, Country country)
+			=> _instance.ValidateNationalIdentityCode(ssn, country);
+
+		public static ValidationResult ValidateZIPCode(string zip, Country country)
+			=> _instance.ValidateZIPCode(zip, country);
+	}
+}
