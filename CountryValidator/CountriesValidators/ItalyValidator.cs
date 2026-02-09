884c131971b2647d4eb8490fc1745f11a0044af3
Steve Aze <90449376+ledgersteve@users.noreply.github.com>
2026-02-09 11:40:00 +0000

    Remove dead code.

--- CountryValidator/CountriesValidators/ItalyValidator.cs
+++ CountryValidator/CountriesValidators/ItalyValidator.cs
@@ -8,9 +8,6 @@ namespace CountryValidation.Countries
     {
         public override Country CountryCode => Country.IT;
 
-        private static readonly string Months = "ABCDEHLMPRST";
-        private static readonly string Vocals = "AEIOU";
-        private static readonly string Consonants = "BCDFGHJKLMNPQRSTVWXYZ";
         private static readonly string OmocodeChars = "LMNPQRSTUV";
         private static readonly int[] ControlCodeArray = new[] { 1, 0, 5, 7, 9, 13, 15, 17, 19, 21, 2, 4, 18, 20, 11, 3, 6, 8, 12, 14, 16, 10, 22, 25, 24, 23 };
         private static readonly Regex CheckRegex = new Regex(@"^[A-Z]{6}[\d]{2}[A-Z][\d]{2}[A-Z][\d]{3}[A-Z]$");
