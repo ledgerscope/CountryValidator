9eb72e104983ed1cba249fcd7c4495fa2eb37453
Steve Aze <90449376+ledgersteve@users.noreply.github.com>
2026-02-10 10:34:01 +0000

    Expose the by-country-code string methods on the interface.

--- CountryValidator/ICountryValidator.cs
+++ CountryValidator/ICountryValidator.cs
@@ -4,11 +4,21 @@ namespace CountryValidation
 {
     public interface ICountryValidator
     {
+        ValidationResult ValidateEntity(string vat, string countryCode);
         ValidationResult ValidateEntity(string vat, Country country);
+
+        ValidationResult ValidateIndividualTaxCode(string id, string countryCode);
         ValidationResult ValidateIndividualTaxCode(string id, Country country);
+
+        ValidationResult ValidateNationalIdentityCode(string ssn, string countryCode);
         ValidationResult ValidateNationalIdentityCode(string ssn, Country country);
+
+        ValidationResult ValidateVAT(string vat, string countryCode);
         ValidationResult ValidateVAT(string vat, Country country);
+
+        ValidationResult ValidateZIPCode(string zip, string countryCode);
         ValidationResult ValidateZIPCode(string zip, Country country);
+
         bool TryGetCountryByCode(string code, out Country country);
         bool IsCountrySupported(string countryCode);
         bool IsCountrySupported(Country country);
